6c3248558af1a5b3f6c1953e8b34ed3d8aeb9151
beastcode9999 <beastcode9999@gmail.com>
2026-03-21 14:35:40 +0930

    feat: add user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('student', 'Student'),
+        ('admin', 'Admin'),
+        ('trainer', 'Trainer'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
