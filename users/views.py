c16c6986ce9ac38658c1f6ab19d7d6909e2b6d2a
beastcode9999 <beastcode9999@gmail.com>
2026-03-23 09:53:38 +0930

    feat: add views for users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .serializers import RegisterSerializer, UserSerializer
+from .models import User
+
+# DRF pre-built view which handles POST requests only
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
